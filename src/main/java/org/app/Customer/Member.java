b7476e4c5ec190d8563653c2a29a815a57c33303
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2023-05-06 16:55:00 +0700

    fix: customer bills parameters

--- src/main/java/org/app/Customer/Member.java
+++ src/main/java/org/app/Customer/Member.java
@@ -2,10 +2,12 @@ package org.app.Customer;
 
 import java.util.List;
 
+import org.app.Inventory.Holder.FixedBill;
+
 class Member extends RegisteredCustomer {
     private final double poin_rate = 0.01;
 
-    public Member(int id, List<Integer> bills, String name, String telephoneNumber, int point, boolean active) {
+    public Member(int id, List<FixedBill> bills, String name, String telephoneNumber, int point, boolean active) {
         super(id, bills, name, telephoneNumber, point, active);
     }
 }
--- src/main/java/org/app/Customer/VIP.java
+++ src/main/java/org/app/Customer/VIP.java
@@ -2,10 +2,12 @@ package org.app.Customer;
 
 import java.util.List;
 
+import org.app.Inventory.Holder.FixedBill;
+
 class VIP extends Member {
     private final double diskon = 0.1;
 
-    public VIP(int id, List<Integer> bills, String name, String telephoneNumber, int point, boolean active) {
+    public VIP(int id, List<FixedBill> bills, String name, String telephoneNumber, int point, boolean active) {
         super(id, bills, name, telephoneNumber, point, active);
     }
 }
