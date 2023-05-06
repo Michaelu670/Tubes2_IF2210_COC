9e00dacdaad56ffe5312b61e8c3257f855961c37
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2023-05-06 20:00:07 +0700

    fix redundant getter setter

--- src/main/java/org/app/Customer/RegisteredCustomer.java
+++ src/main/java/org/app/Customer/RegisteredCustomer.java
@@ -5,14 +5,12 @@ import lombok.Setter;
 import org.app.Inventory.Holder.FixedBill;
 
 import java.util.List;
+
+@Getter @Setter
 abstract class RegisteredCustomer extends Customer {
-    @Getter @Setter
     private String name;
-    @Getter @Setter
     private String telephoneNumber;
-    @Getter @Setter
     private double point;
-    @Getter @Setter
     private boolean active;
 
     public RegisteredCustomer(int id, List<FixedBill> bills, String name, String telephoneNumber, double point, boolean active) {
