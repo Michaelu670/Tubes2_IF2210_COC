c7ab6eaf589fc39d3898a9ee95194ddad05cb3ef
BrianKheng <briankheng63@gmail.com>
2023-05-08 04:32:44 +0700

    refactor: remove unused method & attribute

--- src/main/java/org/app/Plugin/PluginManager.java
+++ src/main/java/org/app/Plugin/PluginManager.java
@@ -1,8 +1,5 @@
 package org.app.Plugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.app.DataStore.DataHolder;
 
 import jakarta.xml.bind.annotation.XmlAccessorType;
@@ -14,20 +11,11 @@ import org.app.Setting.Setting;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class PluginManager extends DataHolder {
-    private static List<String> filePaths = new ArrayList<>();
-
     public static void register(String filePath, MainGUI mainGUI) {
-        // Add file to db
-        filePaths.add(filePath);
         PluginLoader.load(filePath, mainGUI);
     }
 
-    public static void remove(String filePath) {
-        // Delete file from db, then reload all plugin
-    }
-
     public static void enableAll(MainGUI mainGUI) {
-        // Get all file from db, then load all plugin
         for (String filePath : Setting.getInstance().getPluginPaths()) {
             PluginLoader.load(filePath, mainGUI);
         }
