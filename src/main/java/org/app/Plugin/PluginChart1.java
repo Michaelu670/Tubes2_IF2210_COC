06f90a9ece8384185a4b67b6374ca9064b24f6ed
BrianKheng <briankheng63@gmail.com>
2023-05-07 23:11:51 +0700

    feat: add plugin chart 1

---
+++ src/main/java/org/app/Plugin/PluginChart1.java
@@ -0,0 +1,59 @@
+package org.app.Plugin;
+
+import org.app.DataStore.DataStoreInterface;
+import org.app.GUI.MainGUI;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+
+public class PluginChart1 extends BasePlugin implements Plugin, Runnable {
+    @Override
+    public void onEnable(MainGUI mainGUI, DataStoreInterface dataStore) {
+        this.mainGUI = mainGUI;
+        this.dataStore = dataStore;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        mainGUI.newTabAction(panel).actionPerformed(null);
+        while (true) {
+            try {
+                // Panel initialization
+                panel.removeAll();
+                panel.setLayout(new java.awt.GridLayout(1, 2));
+
+                // Create Line Chart
+                JFreeChart lineChart = ChartFactory.createLineChart(
+                        "Line Chart",
+                        "Item", "Number of Item Stock",
+                        createCategoryDataset(),
+                        org.jfree.chart.plot.PlotOrientation.VERTICAL,
+                        true, true, false);
+
+                ChartPanel lineChartPanel = new ChartPanel(lineChart);
+                lineChartPanel.setPreferredSize(new java.awt.Dimension(500, 400));
+
+                // Create Bar Chart
+                JFreeChart barChart = ChartFactory.createBarChart(
+                        "Bar Chart",
+                        "Item", "Number of Item Stock",
+                        createCategoryDataset(),
+                        org.jfree.chart.plot.PlotOrientation.VERTICAL,
+                        true, true, false);
+
+                ChartPanel barChartPanel = new ChartPanel(barChart);
+                barChartPanel.setPreferredSize(new java.awt.Dimension(500, 400));
+
+                // Add to panel
+                panel.add(lineChartPanel);
+                panel.add(barChartPanel);
+
+                // Sleep 10s
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
