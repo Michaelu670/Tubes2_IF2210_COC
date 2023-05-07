dcb48a8cc2bbab03fa2b970239dc9f554e2f555c
BrianKheng <briankheng63@gmail.com>
2023-05-07 21:37:36 +0700

    feat: add threading

--- src/main/java/org/app/Plugin/BasePlugin.java
+++ src/main/java/org/app/Plugin/BasePlugin.java
@@ -4,53 +4,43 @@ package org.app.Plugin;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
+import org.app.DataStore.DataStoreInterface;
+import org.app.GUI.MainGUI;
+import org.app.Inventory.Holder.Inventory;
+
 public abstract class BasePlugin {
-    protected static JPanel panel = null;
+    protected JPanel panel;
+    protected MainGUI mainGUI;
+    protected DataStoreInterface dataStore;
     protected static List<String> data; // Data from db
 
     public BasePlugin() {
-        if (panel == null) {
-            panel = new JPanel();
-            // Get data from db
-            data = new ArrayList<>(List.of("coffee", "bread", "coffee", "porridge", "tea", "tea")); // Temporary data
-        }
+        panel = new JPanel();
+        // Get data from db
+        data = new ArrayList<>(List.of("coffee", "porridge", "tea")); // Temporary data
     }
 
-    public org.jfree.data.category.DefaultCategoryDataset createCategoryDataset() {
+    public org.jfree.data.category.DefaultCategoryDataset createCategoryDataset(Inventory inventory) {
+        System.out.println(inventory.itemList().size());
+        System.out.println(inventory.itemList());
         org.jfree.data.category.DefaultCategoryDataset dataset = new org.jfree.data.category.DefaultCategoryDataset();
         for (int i = 0; i < data.size(); i++) {
-            dataset.addValue(Collections.frequency(data, data.get(i)), "Count", data.get(i));
+            Random random = new Random();
+            dataset.addValue(random.nextInt(30), "Count", data.get(i));
         }
         return dataset;
     }
 
-    public org.jfree.data.general.PieDataset createPieDataset() {
-        org.jfree.data.general.DefaultPieDataset dataset = new org.jfree.data.general.DefaultPieDataset();
-        for (int i = 0; i < data.size(); i++) {
-            dataset.setValue(data.get(i), Collections.frequency(data, data.get(i)));
-        }
-        return dataset;
-    }
+    // public org.jfree.data.general.PieDataset createPieDataset() {
+    //     org.jfree.data.general.DefaultPieDataset dataset = new org.jfree.data.general.DefaultPieDataset();
+    //     for (int i = 0; i < data.size(); i++) {
+    //         dataset.setValue(data.get(i), Collections.frequency(data, data.get(i)));
+    //     }
+    //     return dataset;
+    // }
 
-    // Use thread to update data
-    // @Override
-    // public void run() {
-    // while (true) {
-    // // Get updated data from db
-    // try {
-    // Thread.sleep(10000);
-    // data = new ArrayList<>(List.of("1", "2", "2", "3", "3", "3"));
-    // try {
-    // Thread.sleep(10000);
-    // } catch (InterruptedException e) {
-    // e.printStackTrace();
-    // }
-    // } catch (InterruptedException e) {
-    // e.printStackTrace();
-    // }
-    // }
-    // }
 }
--- src/main/java/org/app/Plugin/Plugin.java
+++ src/main/java/org/app/Plugin/Plugin.java
@@ -1,9 +1,10 @@
 package org.app.Plugin;
 
 import org.app.DataStore.DataStoreInterface;
+import org.app.GUI.MainGUI;
 
 import javax.swing.*;
 
 public interface Plugin {
-    void onEnable(JFrame mainGUI, DataStoreInterface dataStore);
+    void onEnable(MainGUI mainGUI, DataStoreInterface dataStore);
 }
