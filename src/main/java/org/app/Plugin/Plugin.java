da14fce00586c0a230510b86ecba70d64237a59b
BrianKheng <briankheng63@gmail.com>
2023-05-07 21:55:25 +0700

    fix: plugin interface, revert to MainGUI

--- src/main/java/org/app/Plugin/Plugin.java
+++ src/main/java/org/app/Plugin/Plugin.java
@@ -3,8 +3,6 @@ package org.app.Plugin;
 import org.app.DataStore.DataStoreInterface;
 import org.app.GUI.MainGUI;
 
-import javax.swing.*;
-
 public interface Plugin {
     void onEnable(MainGUI mainGUI, DataStoreInterface dataStore);
 }
