1364467f14043face85f5c6c9b579f5b2c914568
yansans <66671259+yansans@users.noreply.github.com>
2023-05-07 20:49:47 +0700

    feat: Inventory System Buy Order template

---
+++ src/main/java/org/app/GUI/Form/InventorySystemBuyOrder.java
@@ -0,0 +1,13 @@
+package org.app.GUI.Form;
+
+import javax.swing.*;
+
+public class InventorySystemBuyOrder {
+    private JTextField numfieldTextField;
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JButton button2;
+    private JTextArea textArea1;
+    private JButton cancelButton;
+    private JButton OKButton;
+}
